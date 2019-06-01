d5252b75bbd8ec09ae7851ab9b020618011e1570
Nick <nikitamm59rus@gmail.com>
2019-06-01 10:06:43 +0300

    Add project files.

---
+++ ParseMusicEnergy_v0/Program.cs
@@ -0,0 +1,88 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ParseMusicEnergy_v0
+{
+    class Program
+    {        
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                string url = "";
+                do
+                    url = await Start2();
+                while (url == "");
+
+                using (var mf = new MediaFoundationReader(url))
+                using (var wo = new WaveOutEvent())
+                {
+                    wo.Init(mf);
+                    wo.Volume = 0.35f;
+                    wo.Play();
+                    while (wo.PlaybackState == PlaybackState.Playing)
+                    {
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n\n\n" + ex.Message + "\n\n\n");
+                Console.WriteLine("Now, I'm trying more.");
+                await Task.Delay(2000);
+            }
+
+            await Main(args);
+        }
+
+        static async Task<string> Start2()
+        {
+            try
+            {
+                string response = await new HttpClient().GetStringAsync("http://www.energyfm.ru/");
+                Console.WriteLine("(1/5) -- Energy html getted");
+                string url = System.Text.RegularExpressions.Regex.Match(response, "data-playlist=\"([^\"]+)\"").Groups[1].Value;
+                Console.WriteLine("(2/5) -- Audio tag found");
+                response = await new HttpClient().GetStringAsync(url);
+                Console.WriteLine("(3/5) -- RootObject getted");
+                RootObject root = Newtonsoft.Json.JsonConvert.DeserializeObject<RootObject>(response);
+                Console.WriteLine("(4/5) -- RootObject converted");
+                url = (root.playlist.Where(p => p.file[9] == '7').SingleOrDefault()).file;
+                Console.WriteLine("(5/5) -- ic7 url found");
+
+                return url;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("\n\n\n" + ex.Message + "\n\n\n");
+                Console.WriteLine("Now, I'm trying more.");
+                await Task.Delay(2000);
+                return "";
+            }
+        }
+    }
+
+    public class Playlist
+    {
+        public string comment { get; set; }
+        public string file { get; set; }
+    }
+
+    public class RootObject
+    {
+        public List<Playlist> playlist { get; set; }
+    }
+}
+
+//string response = await new HttpClient().GetStringAsync("https://www.energyfm.ru/");
+//var energy = new HtmlDocument();
+//energy.LoadHtml(response.Trim());
+//var str2 = energy.DocumentNode.Descendants("audio");
+//var x = energy.DocumentNode.SelectNodes("//audio[@data-playlist]").Select(n => n.Attributes["data-playlist"].Value).SingleOrDefault();
+
+//"http://ic7.101.ru:8000/a99?userid=0&setst=satb6cgskuu3emni9713hcfsvm" --- 9 symbol == 7
---
+++ README.md
@@ -0,0 +1,7 @@
+# Radio-Energy-Parsing
+### A simple project for parsing audio stream. This script will play music from radio Energy
+  
+## How to use:
+- Clone the repo and view code
+- Clone only "Music here" folder. Add link whatever you want. Click link if you want the music.  
+## Enjoy the app!
