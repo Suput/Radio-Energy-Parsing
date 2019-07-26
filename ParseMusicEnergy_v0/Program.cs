561ecf66afd3daf0483e5300820d0089124abf10
Nick <nikitamm59rus@gmail.com>
2019-07-26 19:13:28 +0300

    Console window hides (not in background) just after starting

--- ParseMusicEnergy_v0/Program.cs
+++ ParseMusicEnergy_v0/Program.cs
@@ -6,16 +6,31 @@ using System.Net.Http;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace ParseMusicEnergy_v0
 {
     class Program
     {
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SW_HIDE = 6;
+        const int SW_BACKGROUND = 0;
+        const int SW_SHOW = 5;
+
         static string url = "";
         static int count = 0;
 
         static async Task Main(string[] args)
         {
+            var handle = GetConsoleWindow();
+            // Hide
+            ShowWindow(handle, SW_HIDE);
+
             while (true)
             {
                 await Start();
